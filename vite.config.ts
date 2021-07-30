84edbd09063fbc08cde94fdcb9d6217872ea6219
orzi! <1063614727@qq.com>
2021-07-30 14:25:29 +0800

    更新配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src")
+    }
+  },
   plugins: [reactRefresh()]
 })
